ff3408681ffcaffd83fac77c4b90ca5d765b6407
Tabriji <tabrijiummi@gmail.com>
2024-06-22 22:27:28 +0600

    mm

---
+++ src/main/java/com/tabriji/dataTypeOperatorAndExpression/NumberSystemConversion.java
@@ -0,0 +1,15 @@
+package com.tabriji.dataTypeOperatorAndExpression;
+
+public class NumberSystemConversion {
+    public static void main(String[] args) {
+        int number = 1_000_000; // 1 million
+
+        String binary = Integer.toBinaryString(number);
+        String hex = Integer.toHexString(number);
+        String octal = Integer.toOctalString(number);
+
+        System.out.println("Binary: " + binary);
+        System.out.println("Hex Decimal: " + hex);
+        System.out.println("Octal: " + octal);
+    }
+}
---
+++ src/main/java/com/tabriji/dataTypeOperatorAndExpression/TextBlockDemo.java
@@ -0,0 +1,11 @@
+package com.tabriji.dataTypeOperatorAndExpression;
+
+public class TextBlockDemo {
+    public static void main(String[] args) {
+        String paragraph = """
+                The roaring fans saw
+                their beloved "Tigers" create another
+                \tbit of history in their favorite den last night.""";
+        System.out.println(paragraph);
+    }
+}
