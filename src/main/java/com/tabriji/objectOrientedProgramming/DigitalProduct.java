d4949a100f04cf0fcd5f54d2839f008a4fab0844
Tabriji <tabrijiummi@gmail.com>
2024-12-31 01:30:47 +0600

    commit

---
+++ src/main/java/com/tabriji/objectOrientedProgramming/DigitalProduct.java
@@ -0,0 +1,7 @@
+package com.tabriji.objectOrientedProgramming;
+
+public class DigitalProduct extends Product {
+    public DigitalProduct(String name, int weight, Dimension dimension, double price, int discount) {
+        super(name, weight, dimension, price, discount);
+    }
+}
